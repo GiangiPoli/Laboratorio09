28ecd24c9f5a488a2a44e96d420c0232857d3937
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2025-11-17 13:24:28 +0100

    Controller Class Implemented

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,67 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    
+    // Class Fields
+    private final static String DEFAULT_PATH = System.getProperty("user.home")
+    + File.pathSeparator
+    + "output.txt";
+    private File file;
 
+    // Class Constructor
+    /**
+     * Using by default output.txt saved in DEFAULT_PATH
+     */
+    public Controller() {
+        this.file = new File(DEFAULT_PATH);
+    }
+
+    // Class Method
+
+    /**
+     * A method for setting a File as current file
+     * @param file contain the new current file to write in
+     */
+    public void setCurrentFile(File file) {
+        this.file = file;
+    }
+
+    /**
+     * A method for getting the current File
+     * @return the current file
+     */
+    public File getCurrentFile() {
+        return this.file;
+    }
+
+    /**
+     * A method for getting the path (in form of String)
+     * of the current File
+     * @return the path in String form
+     */
+    public String getFilePath() {
+        return file.getAbsolutePath();
+    }
+
+    /**
+     * A method that gets a String as input and saves its content 
+     * on the current file. This method may throw an IOException.
+     * @param content contains what i want to add in the file
+     */
+    public void writeOnFile(String content) {
+        try (PrintStream ps = new PrintStream(file, StandardCharsets.UTF_8)) {
+            ps.println(content);
+        } catch (final IOException e) {
+            System.out.println("EXCEPTION GENERATED -> " 
+            + e.getMessage()); //NOPMD
+        }
+    }
 }
