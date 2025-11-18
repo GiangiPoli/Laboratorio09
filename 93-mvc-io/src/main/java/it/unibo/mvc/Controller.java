b97361116b19cfcbf765824f7326064c388b6007
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2025-11-18 15:33:43 +0100

    Controller Interface and its implentation done

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,36 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    /**
+     * This method allows you to set thr next string to be outputted.
+     * 
+     * @param next contains the next string to be outputted.
+     */
+    public void setNextString(final String next);
+
+    /**
+     * This method allows you to get the next string to be outputted.
+     * 
+     * @return the next string to be outputted.
+     */
+    public String getNextString(); 
+    /**
+     * This method allows you to write something on std out.
+     * 
+     * @param output contains what u want to display on std out.
+     */
+    public void writeOnStdOut(final String output);
+
+    /**
+     * This method allows you to get all the previous string outputted.
+     * 
+     * @return a list of the previous outputted strings.
+     */
+    public List<String> getOuputHistory();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,61 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
+ * Simple controller implementation.
  * 
- *
  */
 public final class SimpleController implements Controller {
 
+    // Class Fields
+    private final List<String> previousOutput = new LinkedList<>();
+    private String nextToBeOutput;
+
+    // Class Constructor
+    /**
+     * Empty Constructor.
+     */
+    public SimpleController() {
+        /*
+         * It does nothing
+         */
+    }
+
+    // Class Method
+
+    /**
+     * @{inheritDoc}.
+     */
+    @Override
+    public void setNextString(String next) {
+        this.nextToBeOutput = next;
+    }
+
+    /**
+     * @{inheritDoc}.
+     */
+    @Override
+    public String getNextString() {
+        return this.nextToBeOutput;
+    }
+
+    /**
+     * @{inheritDoc}.
+     */ 
+    @Override
+    public void writeOnStdOut(String output) {
+        System.out.println(this.nextToBeOutput); //NOPMD
+        previousOutput.add(this.nextToBeOutput);
+    }
+
+    /**
+     * @{inheritDoc}.
+     */
+    @Override
+    public List<String> getOuputHistory() {
+        return this.previousOutput;
+    }
+    
 }
